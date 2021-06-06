c719863fb3063307934dccac06ea458b15b03de1
Eldeen-will <74831639+Eldeen-will@users.noreply.github.com>
2021-06-05 22:53:43 -0300

    Add files via upload

--- ecma.js
+++ ecma.js
@@ -1,24 +1,24 @@
 let main = document.getElementById('main');
 
 let homeM = document.getElementById('home');
-let itemsM = document.getElementById('items');
-let usM = document.getElementById('us');
-let depoiM = document.getElementById('depoi');
-let socialM = document.getElementById('social');
-let contactM = document.getElementById('contact');
+let edreM = document.getElementById('edre');
+let tapeM = document.getElementById('tape');
+let bandaM = document.getElementById('banda');
+let mantaM = document.getElementById('manta');
+let traveM = document.getElementById('trave');
 
-let homeA = document.getElementById('h');
-let itemsA = document.getElementById('i');
-let usA = document.getElementById('u');
-let depoiA = document.getElementById('d');
-let socialA = document.getElementById('s');
-let contactA = document.getElementById('c');
+let homeA = document.getElementById('ho')
+let edreA = document.getElementById('ed');
+let tapeA = document.getElementById('ta');
+let bandaA = document.getElementById('ba');
+let mantaA = document.getElementById('ma');
+let traveA = document.getElementById('tr')
 
 
 main.addEventListener('click', function(e) {
     let escolha = e.target.id;
-    let choices1 = [homeM, itemsM, usM, depoiM, socialM, contactM];
-    let choices2 = [homeA, itemsA, usA, depoiA, socialA, contactA];
+    let choices1 = [homeM, edreM, tapeM, bandaM, mantaM, traveM];
+    let choices2 = [homeA, edreA, tapeA, bandaA, mantaA, traveA];
 
     for(i=0; i<6; i++){
         let reset1 = choices1[i];
@@ -33,32 +33,31 @@ main.addEventListener('click', function(e) {
             homeA.className = 'activeA';
         break;
 
-        case 'items':
-            itemsM.className = 'activeM';
-            itemsA.className = 'activeA';
+        case 'edre':
+            edreM.className = 'activeM';
+            edreA.className = 'activeA';
         break;
 
-        case 'us':
-            usM.className = 'activeM';
-            usA.className = 'activeA';
+        case 'tape':
+            tapeM.className = 'activeM';
+            tapeA.className = 'activeA';
         break;
 
-        case 'depoi':
-            depoiM.className = 'activeM';
-            depoiA.className = 'activeA';
+        case 'banda':
+            bandaM.className = 'activeM';
+            bandaA.className = 'activeA';
         break;
 
-        case 'social':
-            socialM.className = 'activeM';
-            socialA.className = 'activeA';
+        case 'manta':
+            mantaM.className = 'activeM';
+            mantaA.className = 'activeA';
         break;
 
-        case 'contact':
-            contactM.className = 'activeM';
-            contactA.className = 'activeA';
+        case 'trave':
+            traveM.className = 'activeM';
+            traveA.className = 'activeA';
         break;
+
        };
-
-main();
        
 });
