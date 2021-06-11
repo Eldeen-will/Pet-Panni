507c86bf75f30933b88f657720837b81b0b561f5
Eldeen-will <74831639+Eldeen-will@users.noreply.github.com>
2021-06-11 20:49:44 -0300

    Add files via upload

--- ecma.js
+++ ecma.js
@@ -1,5 +1,3 @@
-let menu = document.getElementById('menu');
-
 let homeM = document.getElementById('home');
 let edreM = document.getElementById('edre');
 let tapeM = document.getElementById('tape');
@@ -12,56 +10,61 @@ let edreA = document.getElementById('ed');
 let tapeA = document.getElementById('ta');
 let bandaA = document.getElementById('ba');
 let mantaA = document.getElementById('ma');
-let traveA = document.getElementById('tr')
+let traveA = document.getElementById('tr');
 
+let botoes = document.querySelectorAll('.button');
 
-menu.addEventListener('click', function(e) {
-    let escolha = e.target.id;
-    let choices1 = [homeM, edreM, tapeM, bandaM, mantaM, traveM];
-    let choices2 = [homeA, edreA, tapeA, bandaA, mantaA, traveA];
+botoes.forEach((botao) => {
+    botao.addEventListener('click', function(e) {
+        let escolha = e.target.id;
+        let choices1 = [homeM, edreM, tapeM, bandaM, mantaM, traveM];
+        let choices2 = [homeA, edreA, tapeA, bandaA, mantaA, traveA];
 
-    for(i=0; i<6; i++){
-        let reset1 = choices1[i];
-        let reset2 = choices2[i];
-        reset2.classList.remove('activeA');
-        reset2.classList.add('disable');
-        reset1.classList.remove('activeM');
-    };
+        for(i=0; i<6; i++){
+            let reset1 = choices1[i];
+            let reset2 = choices2[i];
+            reset2.classList.remove('activeA');
+            reset2.classList.add('disable');
+            reset1.classList.remove('activeM');
+        };
 
-       switch(escolha){
+        switch(escolha){
 
-        case 'home':
-            homeM.className = 'activeM';
-            homeA.className = 'activeA';
-        break;
+            case 'home':
+                homeM.className = 'activeM';
+                homeA.className = 'activeA';
+            break;
 
-        case 'edre':
-            edreM.className = 'activeM';
-            edreA.className = 'activeA';
-        break;
+            case 'edre':
+                edreM.className = 'activeM';
+                edreA.className = 'activeA';
+            break;
 
-        case 'tape':
-            tapeM.className = 'activeM';
-            tapeA.className = 'activeA';
-        break;
+            case 'tape':
+                tapeM.className = 'activeM';
+                tapeA.className = 'activeA';
+            break;
 
-        case 'banda':
-            bandaM.className = 'activeM';
-            bandaA.className = 'activeA';
-        break;
+            case 'banda':
+                bandaM.className = 'activeM';
+                bandaA.className = 'activeA';
+            break;
 
-        case 'manta':
-            mantaM.className = 'activeM';
-            mantaA.className = 'activeA';
-        break;
+            case 'manta':
+                mantaM.className = 'activeM';
+                mantaA.className = 'activeA';
+            break;
 
-        case 'trave':
-            traveM.className = 'activeM';
-            traveA.className = 'activeA';
-        break;
-       };
-       
-});
+            case 'trave':
+                traveM.className = 'activeM';
+                traveA.className = 'activeA';
+            break;
+        };
+
+    });
+
+}); 
+
 
 let check = document.getElementById("check")
 
